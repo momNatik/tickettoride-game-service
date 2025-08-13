3f59d80801f46b3d7db3f62d08e4e7b3b9301c52
Nataliya <mom.natik@gmail.com>
2025-08-13 15:02:31 +0300

    fixes

--- src/app.mjs
+++ src/app.mjs
@@ -1,24 +1,30 @@
-import { ConnectToQueue } from "../tickettoride-backend-common/src/queue/consumption.mjs";
+import { ConnectToQueueAsync } from "../tickettoride-backend-common/src/queue/consumption.mjs";
 import {
   OpenChannelAsync,
   SendMessageToQueue,
 } from "../tickettoride-backend-common/src/queue/publisher.mjs";
 
-const options = {
-  exchangeName: process.env.EXCHANGE_NAME,
-  queueName: process.env.QUEUE_NAME,
-};
+let mapBackgroundServiceChannelInfo;
 
-const mapBackgroundServiceQueueOptions = {
-  exchangeName: process.env.EXCHANGE_NAME_BACKGROUND,
-  queueName: process.env.QUEUE_NAME_BACKGROUND,
-};
+Main();
 
-ConnectToQueue(options, ProcessMessageAsync);
+async function Main() {
+  const options = {
+    exchangeName: process.env.EXCHANGE_NAME,
+    queueName: process.env.QUEUE_NAME,
+  };
 
-const mapBackgroundServiceChannelInfo = OpenChannelAsync(
-  mapBackgroundServiceQueueOptions
-);
+  const mapBackgroundServiceQueueOptions = {
+    exchangeName: process.env.EXCHANGE_NAME_BACKGROUND,
+    queueName: process.env.QUEUE_NAME_BACKGROUND,
+  };
+
+  await ConnectToQueueAsync(options, ProcessMessageAsync);
+
+  mapBackgroundServiceChannelInfo = await OpenChannelAsync(
+    mapBackgroundServiceQueueOptions
+  );
+}
 
 async function ProcessMessageAsync(msg) {
   const message = GetMapBackgroundServiceMessage(msg);
