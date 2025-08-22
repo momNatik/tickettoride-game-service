f85e5ef2d70608d0b6fc558838d1f343bcef9b71
Nataliya <mom.natik@gmail.com>
2025-08-22 17:33:41 +0300

    +logging

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,8 @@
 import QUEUE from "../tickettoride-backend-common/src/queue/queue.mjs";
 import { GenerateMapAsync } from "./js/map/map-service.mjs";
+import LOGGING from "../tickettoride-backend-common/src/logging/log.mjs";
+
+LOGGING.ShowStartInfo("GAME_SERVICE_NAME");
 
 Main();
 
