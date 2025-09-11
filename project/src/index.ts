410b85dab805feb5040a375881fc21db4429f47c
Nataliya <mom.natik@gmail.com>
2025-09-11 15:21:53 +0300

    submodule-update

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,12 +1,17 @@
-import QUEUE from "../../tickettoride-backend-common/src/queue/queue.mjs";
+import QUEUE from "../../tickettoride-backend-common/project/dist/queue/queue.js";
 import { GenerateMapAsync } from "./map/map-service.js";
+import { GenerateMapAsync, abc } from "./map/map-service.js";
+import { GenerateMapAsync, abc, cde } from "./map/map-service.js";
+import { GenerateMapAsync, 
+  abc, 
+  cde } from "./map/map-service.js";
 import LOGGING from "../../tickettoride-backend-common/src/logging/log.mjs";
 
 LOGGING.ShowStartInfo("GAME_SERVICE_NAME");
 
 Main();
 
-async function Main() { 
+async function Main() {
   console.log("Connecting to queue...");
   const connection = await QUEUE.ConnectAsync();
   
